216cdd576ecbe740e10e14cf45d495671848a399
Anna Leach <anna.leach@guardian.co.uk>
2022-09-06 14:43:24 +0100

    tweak comment

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,3 +1,3 @@
 // if you want to import a module from shared/js then you can
 // just do e.g. import Scatter from "shared/js/scatter.js"
-// import {checkAppForDarkMode} from "shared/darkmodecheck.js" - if adding dm to an iframed atom
+// import {checkAppForDarkMode} from "shared/darkmodecheck.js" - if adding dark mode to an iframed atom. Not needed if using the interactive template
