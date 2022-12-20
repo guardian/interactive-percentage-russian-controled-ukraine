72eef9b8b43393ba07281c56d3e0754f5f1bb15f
Pablo Gutierrez <blosky@gmail.com>
2022-12-20 14:16:17 +0000

    correct folders

---
+++ clip.sh
@@ -0,0 +1,47 @@
+
+
+#reproject
+
+for i in *
+do
+for d in $i/*
+do
+
+#echo $d
+#echo $d/*.shp
+mapshaper $d/*.shp -proj wgs84 -o $d/reprojected.json format=geojson
+
+done
+done
+
+
+#clip topojson
+
+for i in *
+do
+for d in $i/*
+do
+
+mapshaper /Users/pablo_gutierrez/Documents/guardian/20221123-interactive-russian-controlled-area/gis/ukraine_map/UKR_adm1.shp -clip  $d/reprojected.json -o $d/clipped.json
+
+done
+done
+
+#make topojson
+
+for i in *
+do
+for d in $i/*
+do
+
+mapshaper $d/*.shp -proj wgs84 -o $d/reprojected-topo.json format=topojson
+
+done
+done
+
+for d in *
+do
+
+mapshaper /Users/pablo_gutierrez/Documents/guardian/20221123-interactive-russian-controlled-area/gis/ukraine_map/UKR_adm1.shp -clip  $d/reprojected.json -o $d/clipped.json
+
+done
