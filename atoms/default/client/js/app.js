ea4a731235432ffc28afc730b74a3cf4881f188e
Pablo Gutierrez <blosky@gmail.com>
2022-12-22 09:38:04 +0000

    full width

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,10 +1,10 @@
 import { Map as mapGl} from 'maplibre-gl';
-//import ukraine from 'assets/OSMB-ea847b38919f684e19312574b67b45941236d33c.json'
-//import oblasts from 'assets/oblasts.json'
+import ukraine from 'assets/geoBoundaries-UKR-ADM0_simplified.json'
+import oblasts from 'assets/oblasts.json'
 import dark from 'assets/gv-dark.json'
 import ScrollyTeller from "shared/js/ScrollyTellerProgress";
 import sheet from 'assets/sheet.json'
-//import labels from 'assets/labels-continent.json'
+import labels from 'assets/labels-continent.json'
 import { merge } from "topojson-client"
 import ukraineControl from 'assets/ukraine-control.json'
 import russiaControl from 'assets/russia-control.json'
@@ -25,9 +25,10 @@ atomEl.style.width = width + "px";
 atomEl.style.height = height + "px";
 
 //
-//dark.sources.oblasts.data = oblasts;
-//dark.sources['ukraine-border'].data = ukraine;
-//dark.sources.labels.data = labels;
+dark.sources.oblasts.data = oblasts;
+dark.sources['ukraine-fill'].data = ukraine;
+dark.sources['ukraine-border'].data = ukraine;
+dark.sources.labels.data = labels;
 dark.sources.overlay.data = null;
 dark.sources['overlay-russia'].data = null;
 dark.sources['overlay-russia-advance'].data = null;
@@ -102,7 +103,7 @@ map.on('load', () => {
 			map.getSource('overlay-russia').setData(topoRussia);
 			map.getSource('overlay-russia-advance').setData(topoRussiaAdvance);
 
-			// document.getElementById('date').innerHTML = currentDate;
+			document.getElementById('date').innerHTML = currentDate;
 
 			// console.log(iniDate , ':', iniPos, '->', endDate , ':', endPos, currentPos, ':', filesDates[currentPos])
 		}
